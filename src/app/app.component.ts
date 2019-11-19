776eb0f0bc4bd1261766a3dcd46f47ba07249dc3
DRandell <jup.trandell@gmail.com>
2019-11-19 19:35:28 +0000

    added initial rating system with sentiment analysis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Sentiment from 'sentiment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'auto-rating';
+  public title = 'auto-rating';
+  public rating = 0;
+  public longRating = 0;
+  public reviewTitle: string;
+  public userReview: string;
+
+  updateRating() {
+    const sentiment = new Sentiment().analyze(`${this.reviewTitle}.  ${this.userReview}`);
+    this.rating = Math.min(5, Math.max(0, sentiment.score));
+    this.longRating = Math.min(100, Math.max(0, this.rating * 20));
+  }
 }
